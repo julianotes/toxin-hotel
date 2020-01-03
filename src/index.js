b7fb1c33cc4b70192d0bf49d4ca1888010208275
Julia Luneva <yulia.lunevavl@yandex.ru>
2020-01-03 13:53:46 +0300

    Again

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import "../import-jquery";
-import "./pages/colors-types/colors-types.pug";
-import "./pages/colors-types/colors-types.scss";
 import  "/components/header/header.js";
 import "jquery-ui-dist/jquery-ui.js";
 
