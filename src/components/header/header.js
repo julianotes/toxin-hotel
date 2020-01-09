93cda15736d74d487124445eb7d3dd2d46befeb6
Julia Luneva <yulia.lunevavl@yandex.ru>
2020-01-09 19:27:53 +0300

    Доработка мобильного футера и хедера

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('.header-burger').click(function(event){
-    $('.header-burger, .header-burger__lines').toggleClass('mobile');
+    $('.header-burger, .header-burger__lines, .header-nav__wrap').toggleClass('mobile');
+    $('body').toggleClass('lock');
   });
 });
